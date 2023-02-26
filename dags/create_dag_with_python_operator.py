6a741794abdfec471a4b92ccd40b9dd38580919c
Lester Bryan Ilao <lestertetengilao@gmail.com>
2023-02-26 23:51:05 +0800

    00:58:00

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timedelta
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
 
 default_args = {
     'owner': 'lester',
@@ -11,12 +9,25 @@ default_args = {
 }
 
 
-def greet(name, age):
-    print(f'Hello World! My name is {name} and age I am {age} years old.')
+def greet(ti):
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
+    print(
+        f'Hello World! My name is {first_name} {last_name} and age I am {age} years old.')
+
+
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Lester')
+    ti.xcom_push(key='last_name', value='Ilao')
+
+
+def get_age(ti):
+    ti.xcom_push(key='age', value=22)
 
 
 with DAG(
-    dag_id='dag_with_python_operator_v1',
+    dag_id='dag_with_python_operator_v6',
     default_args=default_args,
     description='Sample DAG with PythonOperator',
     start_date=datetime(2023, 2, 25, 2),
@@ -24,6 +35,20 @@ with DAG(
 ) as dag:
     task1 = PythonOperator(
         task_id='greet',
-        python_callable=greet,
-        op_kwargs={'name': 'Tome', 'age': 22}
+        python_callable=greet
     )
+
+    task2 = PythonOperator(
+        task_id='get_name',
+        python_callable=get_name
+    )
+
+    task3 = PythonOperator(
+        task_id='get_age',
+        python_callable=get_age
+    )
+
+    # task2 >> task1
+    # task3 >> task1
+
+    [task2, task3] >> task1
--- dags/dag_with_taskflow_api.py
+++ dags/dag_with_taskflow_api.py
@@ -1,42 +1,39 @@
 from datetime import datetime, timedelta
-
 from airflow.decorators import dag, task
 
-
 default_args = {
-    'owner': 'coder2j',
+    'owner': 'lester',
     'retries': 5,
     'retry_delay': timedelta(minutes=5)
 }
 
 
-@dag(dag_id='dag_with_taskflow_api_v02',
+@dag(dag_id='dag_with_taskflow_api_v2',
      default_args=default_args,
-     start_date=datetime(2021, 10, 26),
+     start_date=datetime(2023, 2, 25, 2),
      schedule_interval='@daily')
 def hello_world_etl():
 
     @task(multiple_outputs=True)
     def get_name():
         return {
-            'first_name': 'Jerry',
-            'last_name': 'Fridman'
+            'first_name': 'Lester',
+            'last_name': 'Ilao'
         }
 
     @task()
     def get_age():
-        return 19
+        return 22
 
     @task()
     def greet(first_name, last_name, age):
-        print(f"Hello World! My name is {first_name} {last_name} "
-              f"and I am {age} years old!")
+        print(F'Hello World! My name is {first_name} {last_name} and age I am {age} years old.')
 
+    # name = get_name()
     name_dict = get_name()
     age = get_age()
-    greet(first_name=name_dict['first_name'],
-          last_name=name_dict['last_name'],
-          age=age)
+
+    greet(first_name=name_dict['first_name'], last_name=name_dict['last_name'], age=age)
 
 
 greet_dag = hello_world_etl()
